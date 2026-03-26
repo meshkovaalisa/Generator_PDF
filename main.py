bc4d0b454e1a482f11c9b26146eab712ef911a39
Sem4ik27 <remger4@gmail.com>
2026-03-26 19:00:07 +0300

    тестовый

--- main.py
+++ main.py
@@ -43,4 +43,4 @@ async def render(template: UploadFile, data: UploadFile):
 
 if __name__ == "__main__":
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8008, reload=True)
