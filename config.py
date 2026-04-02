ab865894629f05788ad9a79e1b59860604473934
horizon-spark <166980733+horizon-spark@users.noreply.github.com>
2026-04-02 11:53:40 +0300

    Изменил мейн и конфиг для работы на виртуалке линукса

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent
 temp_dir = BASE_DIR / "temp"
 rendered_files_dir = BASE_DIR / "rendered_files"
-soffice_path = r"C:\Program Files\LibreOffice\program\soffice.exe"
+soffice_path = r"/usr/bin/libreoffice"
 
 
--- main.py
+++ main.py
@@ -91,4 +91,4 @@ async def render(template: UploadFile, data: UploadFile) -> FileResponse:
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
