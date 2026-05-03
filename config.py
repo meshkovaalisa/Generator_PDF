77f709a3303743929d598f7e4796d3f5542c1154
horizon-spark <kadarmetov.dn@yandex.ru>
2026-05-03 11:00:32 +0300

    Fix readme.md

--- config.py
+++ config.py
@@ -9,10 +9,6 @@ temp_dir = BASE_DIR / "temp"
 # Директория для хранения готовых сгенерированных файлов (PDF и других форматов)
 rendered_files_dir = BASE_DIR / "rendered_files"
 
-# Путь к исполняемому файлу LibreOffice для конвертации документов в PDF
-# Указан стандартный путь для Windows-установки LibreOffice
-soffice_path = r"C:\Program Files\LibreOffice\program\soffice.exe"
-
 # Директория для хранения JSON-файлов с данными, разбитыми по факультетам
 faculties_dir = BASE_DIR / "faculties"
 
